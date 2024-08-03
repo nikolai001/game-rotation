d786398457d01c5a4612f21655511798f36f4ad4
nikolairaahauge <nira36696@edu.ucl.dk>
2024-08-03 15:34:30 +0200

    Adding tailwind to project, adding light and darkmode toggle, adding components to render game list

---
+++ src/components/GameListComponent.vue
@@ -0,0 +1,25 @@
+<template>
+	<section class="rounded-md dark:bg-slate-700 bg-neutral-200 w-1/2 max-w-96 py-2 px-4 flex flex-col shadow-md">
+		<div class="flex flex-col items-center">
+			<div
+				v-for="game in props.games"
+				:key="game.id"
+				class="dark:text-neutral-100 text-slate-900"
+			>
+				{{ game.name }}
+			</div>
+		</div>
+		<button
+			v-text="'Fresh rotation'"
+			class="self-end bg-green-400 rounded-md px-2 py-1 dark:text-neutral-100 text-slate-900 shadow hover:shadow-md transition-shadow"
+		/>
+	</section>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+	games: Array,
+});
+</script>
---
+++ src/components/ToggleSwitchComponent.vue
@@ -0,0 +1,24 @@
+<template>
+	<label class="mt-2 flex group w-fit items-center ml-auto mr-2">
+		<span
+			class="dark:text-neutral-100 text-slate-900"
+			v-text="isDark ? 'Toggle light mode' : 'Toggle dark mode'"
+		/>
+
+		<input
+			type="checkbox"
+			v-model="isDark"
+			class="peer appearance-none"
+			@click="toggleDark()"
+		/>
+		<span class="w-10 h-4 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:bg-green-400 after:w-3 after:aspect-square after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-5 group-hover:after:translate-x-1"></span>
+	</label>
+</template>
+
+<script setup>
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark();
+
+const toggleDark = useToggle(isDark);
+</script>
